18c711d624e37b9c74a53624fd5128d0ca011fe2
anetczuk <anetczuk@o2.pl>
2022-01-18 22:27:55 +0100

    Optimize CRC8 by using lookup table

---
+++ src/fastcrc/generate_lookup8.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python2
+
+##
+##
+##
+
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname( __file__ )
+
+
+out_path = os.path.join( BASE_DIR, "src", "crchw", "crc8lookup.h" )
+
+
+##
+## Implementation based on http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html#ch44
+##
+def generate_subtable( poly ):
+    table = dict()
+    for div in xrange(0, 0x100):
+        reg = div
+        for _ in xrange(0, 8):
+            if reg & 0x80 != 0:
+                reg <<= 1
+                reg ^= poly
+            else:
+                reg <<= 1
+        table[ div ] = reg & 0xFF
+    return table
+
+
+def write_row( outfile, poly ):
+    subtable = generate_subtable( poly )
+    
+    for sub in xrange(0, 0xFF):
+        item = subtable[sub]
+        outfile.write( str(item) + ", " )
+
+    ## last item
+    item = subtable[0xFF]
+    outfile.write( str(item) + " " )
+
+
+def main():
+#     table = dict()
+#     for poly in xrange(0, 0x100):
+#         table[ poly ] = generate_subtable( poly )
+        
+    ## write output
+    header = """///
+
+#ifndef CRC8LOOKUPTABLE_H_
+#define CRC8LOOKUPTABLE_H_
+
+#include <stdint.h>                             /// int types
+
+
+///
+/// table indexed in following way: [ polynomial ][ registry ]
+/// 2D representation is slightly faster in use than 1D version
+///
+static const uint8_t CRC8_LookupTable[256][256] = { 
+"""
+
+    footer = """};
+
+#endif /* CRC8LOOKUPTABLE_H_ */
+"""
+
+    with open(out_path, 'w') as outfile:
+        outfile.write( header )
+        
+        for poly in xrange(0, 0xFF):
+            outfile.write( "\t{" )
+            write_row( outfile, poly )
+            outfile.write( "},\n" )
+            
+        ## last poly
+        outfile.write( "\t{" )
+        write_row( outfile, 0xFF )
+        outfile.write( "}\n" )
+        
+        outfile.write( footer )
+        
+    print "table written to:", out_path
+
+
+if __name__ == '__main__':
+    ret = main()
+    sys.exit( ret )
--- src/fastcrc/src/crchw/crc8hw.c
+++ src/fastcrc/src/crchw/crc8hw.c
@@ -50,37 +50,64 @@ CRC8Result* CRC8ResultArray_get( CRC8ResultArray* array, const size_t index ) {
 /// =====================================================
 
 
-/**
- * Implementation based on:
- *      https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
- *      http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
- *
- * Performance can be improved by replacing inner loop with "lookup table" for all 256 combinations of "reg" for each polynomial.
- * It can be especially beneficial if the polynomial is fixed or handling large input data.
- */
-/// 'data_buffer' -- container for data -- first dataframe bit is in MSB of buffer first item
-uint8_t hw_crc8_calculate( const uint8_t* data_buffer, const size_t data_size, const uint8_t polynomial, const uint8_t init_reg, const uint8_t xor_val ) {
-    uint8_t reg = init_reg;
-    for ( size_t i = 0; i < data_size; ++i ) {
-        reg ^= data_buffer[i];                      /// xor
-        
-        if ( (reg & 0xFF) == 0 ) {
-            /// there will be no xor-ring with poly -- just shift
-            reg <<= 8;
-            continue ;
-        }
-        
-        for ( uint8_t j = 0; j < 8; ++j ) {
-            if ( (reg & 0x80) != 0 ) {
-                reg = (reg << 1) ^ polynomial;
-            } else {
-                reg <<= 1;
+#define USE_LOOKUP
+
+
+#ifndef USE_LOOKUP
+    /// standard implementation
+    
+    /**
+     * Implementation based on:
+     *      https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+     *      http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+     */
+    /// 'data_buffer' -- container for data -- first dataframe bit is in MSB of buffer first item
+    uint8_t hw_crc8_calculate( const uint8_t* data_buffer, const size_t data_size, const uint8_t polynomial, const uint8_t init_reg, const uint8_t xor_val ) {
+        uint8_t reg = init_reg;
+        for ( size_t i = 0; i < data_size; ++i ) {
+            reg ^= data_buffer[i];                      /// xor
+            
+            if ( (reg & 0xFF) == 0 ) {
+                /// there will be no xor-ring with poly -- just shift
+                reg <<= 8;
+                continue ;
+            }
+            
+            for ( uint8_t j = 0; j < 8; ++j ) {
+                if ( (reg & 0x80) != 0 ) {
+                    reg = (reg << 1) ^ polynomial;
+                } else {
+                    reg <<= 1;
+                }
             }
         }
+    //     printf( "calculation: poly: 0x%X init: 0x%X xor: 0x%X crc: 0x%X\n", polynomial, init_reg, xor_val, reg ^ xor_val );
+        return reg ^ xor_val;    /// & ((1 << 8) - 1)
     }
-//     printf( "calculation: poly: 0x%X init: 0x%X xor: 0x%X crc: 0x%X\n", polynomial, init_reg, xor_val, reg ^ xor_val );
-    return reg ^ xor_val;    /// & ((1 << 8) - 1)
-}
+
+#else
+    /// lookup table implementation
+    
+    #include "crc8lookup.h"
+    
+    
+    /**
+     * Implementation based on:
+     *      https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+     *      http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+     */
+    /// 'data_buffer' -- container for data -- first dataframe bit is in MSB of buffer first item
+    uint8_t hw_crc8_calculate( const uint8_t* data_buffer, const size_t data_size, const uint8_t polynomial, const uint8_t init_reg, const uint8_t xor_val ) {
+        uint8_t reg = init_reg;
+        for ( size_t i = 0; i < data_size; ++i ) {
+            reg ^= data_buffer[i];                      /// xor
+            reg = CRC8_LookupTable[ polynomial ][ reg ];
+        }
+    //     printf( "calculation: poly: 0x%X init: 0x%X xor: 0x%X crc: 0x%X\n", polynomial, init_reg, xor_val, reg ^ xor_val );
+        return reg ^ xor_val;    /// & ((1 << 8) - 1)
+    }
+
+#endif
 
 
 uint8_t hw_crc8_calculate_param( const uint8_t* data_buffer, const size_t data_size, 

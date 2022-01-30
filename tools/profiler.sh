948d976ca95dd43da35188a144a5da62434469b6
anetczuk <anetczuk@o2.pl>
2022-01-30 20:41:05 +0100

    Remove old profile script

--- tools/profiler.sh
+++ tools/profiler.sh
@@ -3,7 +3,15 @@
 set -eu
 
 
-out_file="$(pwd)/out.prof"
+## SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+tmpdir=$(dirname $(mktemp -u))
+timestamp=$(date +%s)
+
+out_file=$(mktemp ${tmpdir}/out.prof.${timestamp}.XXXXXX)
+#out_file="$(pwd)/out.prof"
+
 
 echo "Starting profiler"
 
@@ -13,3 +21,9 @@ python2 -m cProfile -o $out_file $@
 
 echo ""
 echo "View output: pyprof2calltree -k -i $out_file"
+
+### browser based, installation: pip2 install snakeviz
+echo "Borowser-based view output: snakeviz $out_file"
+
+
+##rm ${out_file}

7c1ff6af5005527d701aa82bf80e51bfd347082d
anetczuk <anetczuk@o2.pl>
2022-02-04 00:15:48 +0100

    Rescale images

--- doc/generate_small.sh
+++ doc/generate_small.sh
@@ -37,11 +37,11 @@ convert_files() {
 }
 
 
-### convert $input_name -resize 240 $output_name
+### convert $input_name -resize 320 $output_name
 ### convert $input_name -resize 200x100 $output_name
 ## $1 -- file extension
 convert_images() {
-    local cmd="convert %s -resize 240 %s"
+    local cmd="convert %s -resize 320 %s"
     local file_ext="$1"
     convert_files "$cmd" ".$file_ext" "-small.$file_ext"
 }
@@ -50,7 +50,7 @@ convert_images() {
 ### ffmpeg -hide_banner -loglevel error -y -i $input_name $output_name
 ## convert mp4 to giff
 convert_mp4() {
-    local cmd="ffmpeg -hide_banner -loglevel error -y -i %s -vf scale=240:-1 %s"
+    local cmd="ffmpeg -hide_banner -loglevel error -y -i %s -vf scale=320:-1 %s"
     convert_files "$cmd" ".mp4" "-small.gif"
 }
 
